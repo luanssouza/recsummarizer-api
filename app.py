e5f769b43b258a2ea774b111819fb03204dfa6fb
LuanSSouza <luan.souza27@hotmail.com>
2021-09-16 21:01:17 -0300

    Adding flask-cors

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+
+# App Modules
 from src.error import error_blueprint
 from src.resources import resources_blueprint
 from src.recommender import recommender_blueprint
 from src.item import item_blueprint
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods = ['GET'])
 def hello():
